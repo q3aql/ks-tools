f1675da977ffe8ab1ed7de8ed58349119acc2e13
q3aql <q3aql@openmailbox.org>
2021-03-08 01:49:07 +0100

    Run script ks-tools.cmd for Cygwin/Windows

---
+++ cygwin/ks-tools.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+rem ######################################
+rem # ks-tools for Windows               #
+rem # U. Modificacion: 10-02-2021        #
+rem # Author: q3aql                      #
+rem # Contact: q3aql@protonmail.ch       #
+rem # License: GPL v2.0                  #
+rem ######################################
+set VERSION=6.6
+
+rem # Start ks-tools terminal (bash)
+mkdir home\%USERNAME%
+copy /Y prompt\.bashrc home\%USERNAME%
+copy /Y prompt\.minttyrc home\%USERNAME%
+cls
+set PATH=%PATH%;%~d0%~p0\bin
+mintty
+rem bash
